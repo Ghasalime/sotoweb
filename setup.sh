0edca224842286094fafbb455331e320e1921833
Ghasali <ghasali@ubuntupc.tail4de293.ts.net>
2026-03-27 21:33:06 +0700

    Refine setup.sh for remote one-liner installation

--- setup.sh
+++ setup.sh
@@ -1,22 +1,45 @@
 #!/bin/bash
 
-# SotoWeb Installation Script
+# SotoWeb - One-liner Installer
 # Author: Antigravity
 
-# Set colors
+# Colors
 GREEN='\033[0;32m'
+BLUE='\033[0;34m'
 NC='\033[0m'
 
-echo "Installing SotoWeb..."
+echo -e "${BLUE}>>> SotoWeb Installation Starting...${NC}"
 
-# Make binary executable
-chmod +x "$(pwd)/bin/soto"
+# 1. Dependency Check
+echo -e "Checking for required tools (git, curl)..."
+apt update -qq > /dev/null
+apt install -y -qq git curl > /dev/null
 
-# Symlink to /usr/local/bin
-sudo ln -sf "$(pwd)/bin/soto" /usr/local/bin/soto
+# 2. Installation Path
+INSTALL_PATH="/etc/sotoweb"
+
+# 3. Clone or Update
+if [[ -d "$INSTALL_PATH" ]]; then
+    echo -e "Updating SotoWeb in $INSTALL_PATH..."
+    cd "$INSTALL_PATH" && git pull -q
+else
+    echo -e "Cloning SotoWeb to $INSTALL_PATH..."
+    # Replace the URL below with your actual GitHub Repo URL after you push it
+    # For now, it might point to this local workspace's clone if you were doing it manually
+    # But for the user, it should be: git clone https://github.com/USER/REPO.git $INSTALL_PATH
+    log_warn "Please ensure you update the repo URL in setup.sh after pushing to GitHub!"
+    # Default placeholder
+    git clone https://github.com/ghasali/SotoWeb.git "$INSTALL_PATH" -q
+fi
+
+# 4. Make binary executable
+chmod +x "$INSTALL_PATH/bin/soto"
+
+# 5. Symlink to /usr/local/bin
+ln -sf "$INSTALL_PATH/bin/soto" /usr/local/bin/soto
 
 if [[ $? -eq 0 ]]; then
-    echo -e "${GREEN}SotoWeb has been installed successfully!${NC}"
+    echo -e "${GREEN}>>> SotoWeb has been installed successfully!${NC}"
     echo "You can now run 'sudo soto help' to get started."
 else
     echo "Installation failed. Please run with sudo."
